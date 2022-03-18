808348109775d401f35230d4033d9b0c9a2a83dc
aivanov <>
2022-03-18 16:15:55 +0000

    Update data job: tracking-covid-serezliev-bayern-munich-boza
    Source deleted automatically upon a data job: tracking-covid-serezliev-bayern-munich-boza deletion
    
    Signed-off-by: aivanov

--- tracking-covid-serezliev-bayern-munich-boza/02_create_covid_deaths_europe_daily.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS covid_deaths_europe_daily_serezliev_bayern_munich_boza (
-    obs_date VARCHAR,
-    number_of_deaths INTEGER,
-    country VARCHAR
-)
--- tracking-covid-serezliev-bayern-munich-boza/03_create_covid_cases_deaths_europe_daily.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS covid_cases_deaths_europe_daily_serezliev_bayern_munich_boza (
-    obs_date VARCHAR,
-    country VARCHAR,
-    number_of_covid_cases_daily INTEGER,
-    number_of_covid_deaths_daily INTEGER
-)
