84df18066f2c04998d59f2bd40a583f1aad8353e
aivanov <>
2022-03-25 10:14:05 +0000

    Update data job: tst-jb
    Initial deploy
    
    Signed-off-by: aivanov

---
+++ tst-jb/03_create_yankee_candle_reviews_transformed.sql
@@ -0,0 +1,9 @@
+-- Create a table that will store the transformed Amazon reviews table with 3 columns:
+-- date, number of total negative reviews and number of negative reviews indicating "no scent".
+-- Table naming convention: yankee_candle_reviews_transformed_YOURNAME where replace "YOURNAME" with your name.
+
+CREATE TABLE IF NOT EXISTS yankee_candle_reviews_transformed (
+    date VARCHAR,
+    num_negative_reviews INTEGER,
+    num_no_scent_reviews INTEGER
+)
