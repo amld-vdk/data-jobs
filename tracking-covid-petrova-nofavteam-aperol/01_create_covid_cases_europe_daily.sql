796c8e394e7711f2f276172286d1532ce9aed958
aivanov <>
2022-03-25 09:45:00 +0000

    Update data job: tracking-covid-petrova-nofavteam-aperol
    Source deleted automatically upon a data job: tracking-covid-petrova-nofavteam-aperol deletion
    
    Signed-off-by: aivanov

--- tracking-covid-petrova-nofavteam-aperol/01_create_covid_cases_europe_daily.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS covid_cases_europe_daily_dimira_nofav_boza (
-    obs_date VARCHAR,
-    number_of_cases INTEGER,
-    country VARCHAR
-)
--- tracking-covid-petrova-nofavteam-aperol/02_create_covid_deaths_europe_daily.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS covid_deaths_europe_daily_dimira_nov_boza (
-    obs_date VARCHAR,
-    number_of_deaths INTEGER,
-    country VARCHAR
-)
