9ac5eee590dc8c2fc596b0026dc4b6d3346fe619
aivanov <>
2022-03-25 09:06:58 +0000

    Update data job: tracking-covid-dako-yantra-beer
    Initial deploy
    
    Signed-off-by: aivanov

---
+++ tracking-covid-dako-yantra-beer/.ipynb_checkpoints/01_create_covid_cases_europe_daily-checkpoint.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_cases_europe_daily_ddakov_yantra_beer (
+    obs_date VARCHAR,
+    number_of_cases INTEGER,
+    country VARCHAR
+)
---
+++ tracking-covid-dako-yantra-beer/.ipynb_checkpoints/02_create_covid_deaths_europe_daily-checkpoint.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_deaths_europe_daily_ddakov_yantra_beer (
+    obs_date VARCHAR,
+    number_of_deaths INTEGER,
+    country VARCHAR
+)
