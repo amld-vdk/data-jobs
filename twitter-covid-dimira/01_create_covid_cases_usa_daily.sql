fd72759153d96c924adb21fd955f0a853dc4e6ec
aivanov <>
2022-03-26 13:56:25 +0000

    Update data job: twitter-covid-dimira
    Initial deploy
    
    Signed-off-by: aivanov

---
+++ twitter-covid-dimira/01_create_covid_cases_usa_daily.sql
@@ -0,0 +1,7 @@
+-- Create a table that will store the daily COVID-19 cases in the US with 2 columns: date and number of cases.
+-- Table naming convention: covid_cases_usa_daily_YOURNAME where replace "YOURNAME" with your name.
+
+CREATE TABLE IF NOT EXISTS covid_cases_us (
+    obs_date VARCHAR,
+    number_of_cases INTEGER
+)
