2895deb0ea3a73ae68e4a1896ee32959c34f57e9
aivanov <>
2022-03-26 11:47:03 +0000

    Update data job: tracking-covid-ale-napoli-kombucha
    Initial deploy
    
    Signed-off-by: aivanov

---
+++ tracking-covid-ale-napoli-kombucha/.ipynb_checkpoints/02_create_covid_deaths_europe_daily-checkpoint.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_deaths_europe_daily_ale_napoli_kombucha (
+    obs_date VARCHAR,
+    number_of_deaths INTEGER,
+    country VARCHAR
+)
---
+++ tracking-covid-ale-napoli-kombucha/01_create_covid_cases_europe_daily.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_cases_europe_daily_ale_napoli_kombucha (
+    obs_date VARCHAR,
+    number_of_cases INTEGER,
+    country VARCHAR
+)
