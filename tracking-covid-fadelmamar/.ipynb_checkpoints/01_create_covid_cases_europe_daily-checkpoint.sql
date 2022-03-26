a1442f350f3281c27aed9b007d94147eab05183e
aivanov <>
2022-03-26 11:55:13 +0000

    Update data job: tracking-covid-fadelmamar
    Initial deploy
    
    Signed-off-by: aivanov

---
+++ tracking-covid-fadelmamar/.ipynb_checkpoints/01_create_covid_cases_europe_daily-checkpoint.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_cases_eu_fadelmamar_amld (
+    obs_date VARCHAR,
+    number_of_cases INTEGER,
+    country VARCHAR
+)
---
+++ tracking-covid-fadelmamar/.ipynb_checkpoints/02_create_covid_deaths_europe_daily-checkpoint.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_deaths_eu_fadelmamar_amld (
+    obs_date VARCHAR,
+    number_of_deaths INTEGER,
+    country VARCHAR
+)
