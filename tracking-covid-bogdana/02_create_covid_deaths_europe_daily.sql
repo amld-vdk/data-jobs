a4a4ea6b77a5ec2764dd6ba776397be8be0b12ad
aivanov <>
2022-03-26 11:51:30 +0000

    Update data job: tracking-covid-bogdana
    Initial deploy
    
    Signed-off-by: aivanov

---
+++ tracking-covid-bogdana/02_create_covid_deaths_europe_daily.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS covid_death_europe_daily_bogdana  (
+    obs_date covid_death_europe_daily_bogdana ,
+    number_of_deaths covid_death_europe_daily_bogdana ,
+    country covid_death_europe_daily_bogdana 
+)
---
+++ tracking-covid-bogdana/03_create_covid_cases_deaths_europe_daily.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS covid_cases_death_europe_daily_bogdana  (
+    obs_date VARCHAR,
+    country VARCHAR,
+    number_of_covid_cases_daily covid_cases_death_europe_daily_bogdana ,
+    number_of_covid_deaths_daily covid_cases_death_europe_daily_bogdana 
+)
